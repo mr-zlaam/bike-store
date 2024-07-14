65edb4ae0b371c5726e949d84935762cd79d3631
Zlaam <mrzalaam@gmail.com>
2024-07-14 12:56:56 +0500

    configure prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import {} from "react";
 
 function Home() {
+  const hello = "hello";
   return (
     <>
       <section className="text-red-500">Home</section>
---
+++ src/appwrite/appwrite.ts
@@ -0,0 +1,10 @@
+import { Account, Client } from "appwrite";
+
+export const client = new Client();
+
+client
+  .setEndpoint(process.env.NEXT_PUBLIC_BACKEND_URI as string)
+  .setProject(process.env.NEXT_PUBLIC_PROJECT_ID as string);
+// * Account
+export const account = new Account(client);
+export { ID } from "appwrite";
