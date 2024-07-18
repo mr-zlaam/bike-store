b5745d8f9d424b54d87abc6bc842ffcf5ce8396c
Zlaam <mrzalaam@gmail.com>
2024-07-18 13:45:43 +0500

    create sign in page and sign up page

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -14,48 +14,55 @@ import { Label } from "@/components/ui/label";
 
 export default function Sign_in() {
   return (
-    <Card className="mx-auto max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
-              <Link href="#" className="ml-auto inline-block text-sm underline">
-                Forgot your password?
+    <section className="h-screen  flex items-center">
+      <div className=" w-fit mx-auto">
+        <Card className="mx-auto max-w-sm shadow-lg shadow-foreground/20">
+          <CardHeader>
+            <CardTitle className="text-2xl text-center">Sign in</CardTitle>
+            <CardDescription>
+              Enter your email below to Sign in to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <Link
+                    href="#"
+                    className="ml-auto inline-block text-sm underline"
+                  >
+                    Forgot your password?
+                  </Link>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+              <Button variant="outline" className="w-full">
+                <FcGoogle size={23} className="mx-3" />
+                Login with Google
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <Link href="/auth/sign-up" className="underline">
+                Sign up
               </Link>
             </div>
-            <Input id="password" type="password" required />
-          </div>
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-          <Button variant="outline" className="w-full">
-            <FcGoogle size={25} className="mx-3" />
-            Login with Google
-          </Button>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
-          <Link href="#" className="underline">
-            Sign up
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
+          </CardContent>
+        </Card>
+      </div>
+    </section>
   );
 }
--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,11 +1,71 @@
-import {} from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { FcGoogle } from "react-icons/fc";
 
-function Sign_up() {
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function Sign_in() {
   return (
-    <>
-      <section>Sign_up</section>
-    </>
+    <section className="h-screen  flex items-center">
+      <div className=" w-fit mx-auto">
+        <Card className="mx-auto max-w-sm shadow-lg shadow-foreground/20">
+          <CardHeader>
+            <CardTitle className="text-2xl text-center">Sign up</CardTitle>
+            <CardDescription>
+              Enter your email below to Sign up to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  id="name"
+                  type="text"
+                  placeholder="John Doe"
+                  autoComplete="off"
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  autoComplete="off"
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" type="password" autoComplete="off" />
+              </div>
+              <Button type="submit" className="w-full">
+                Sign up
+              </Button>
+              <Button variant="outline" className="w-full">
+                <FcGoogle size={23} className="mx-3" />
+                Login with Google
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <Link href="/auth/sign-in" className="underline">
+                Sign up
+              </Link>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </section>
   );
 }
-
-export default Sign_up;
